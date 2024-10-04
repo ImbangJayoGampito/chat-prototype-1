dd795935fa1ee19bd163dd4a1643e1e0234b221b
ImbangJayoGampito <imbangjayogampito@gmail.com>
2024-10-04 08:12:44 +0700

    meow

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,36 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const chatApp = "chat-app-v1"
 
-const assets = ["/", 
-    "/src/index.html",
-     "/src/basic_lib.js"
-    , "/src/chat.html"]
+const assets = ["/",
+    "/index.html",
+    "/basic_lib.js"
+    , "/chat.html",
+    '/icon-32.png',
+    '/icon-144.png',
+    '/icon-192.png',
+    '/icon-512.png',
+    , "/chat.js",
+    "/contact.html",
+    "/header.js"
+    ,
+    "/index.html",
+    "/index.js",
+    "/app.js",
+    "/manifest.json",
+    "/styles.css",
+    "/thanks.html"]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(chatApp).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
+
